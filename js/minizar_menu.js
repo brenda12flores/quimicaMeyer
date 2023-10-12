deb314d3315b244eeb944f8bdce2ceb60a803b42
BrendaMontanoFlores <brendaflores970812@gmail.com>
2023-10-11 18:05:05 -0600

    version 2

---
+++ js/minizar_menu.js
@@ -0,0 +1,18 @@
+/*const menu= document.querySelector(".navbar-nav");
+const open_menu=documentquerySelector(".navbar-toggler");
+const close_menu=documentquerySelector(".colapse");
+
+function toggleMenu(){
+    menu.classList.toggle("menu_opened");
+
+}
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click",toggleMenu);
+
+const menuLinks = document.querySelectorAll('.navbar-nav a[href^="#"]');
+menuLinks.forEach(menuLinks =>{
+    menuLinks.addEventListener("click",function(){
+        menu.classList.remove("menu_opened");
+    })
+})*/
